1599979a13a4c815268fd1897153c2bfb7690ded
veitsi <xtfkpi@gmail.com>
2016-04-03 23:53:32 +0300

    flip views

--- app/src/main/java/com/developerstudyjams/veitsi/appboyhunting/MainActivity.java
+++ app/src/main/java/com/developerstudyjams/veitsi/appboyhunting/MainActivity.java
@@ -4,18 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.flip2);
+
     }
 
     public void order(View view) {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.txtStatus);
         quantityTextView.setText("bfgfdgfdgdf");
+    }
+    public void nextView(View view){
+        final ViewFlipper viewFlipper = (ViewFlipper) findViewById(R.id.viewflipper);
+        viewFlipper.showNext();
     }
 }
